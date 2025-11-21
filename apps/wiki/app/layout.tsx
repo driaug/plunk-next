1037fa9966c1810f49719b1a3ae0bcd1af8626e0
Dries Augustyns <inbox@driaug.com>
2025-11-21 15:36:30 +0100

    Font

--- apps/wiki/app/layout.tsx
+++ apps/wiki/app/layout.tsx
@@ -1,7 +1,6 @@
 import './global.css';
 import {DocsLayout} from 'fumadocs-ui/layouts/docs';
 import {RootProvider} from 'fumadocs-ui/provider/next';
-import {Inter} from 'next/font/google';
 import Script from 'next/script';
 import type {ReactNode} from 'react';
 import React from 'react';
@@ -9,14 +8,19 @@ import React from 'react';
 import {baseOptions} from '@/app/layout.config';
 import {source} from '@/lib/source';
 
-const inter = Inter({
-  subsets: ['latin'],
-});
-
 export default function Layout({children}: {children: ReactNode}) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Favicon */}
         <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
